1f858bdac3b45ba64ee6638fe3fa5de441c0e458
Gal <gal.portnoy11@gmail.com>
2025-05-13 13:44:56 +0300

    update the server.js

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
+
+const mongojs = require('mongojs');
+const db = mongojs('mongodb://127.0.0.1:27017/todolistDB', ['categories', 'bookmarks']);
+
 app.use(express.json()); 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json('Hello World!');
 });
 
+app.post('/addCategory', (req, res) => {
+    const category_name = req.body.name;
+    const category = {
+        name: category_name,
+    }
 
+    db.categories.insert({ category }, (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to add category' });
+        } else {
+            res.status(200).json(data);
+        }
+    });
+},);
 
 const PORT = 3000;
 
