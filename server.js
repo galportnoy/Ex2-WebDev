e4b3ec549b94b6da6f550fe5761dcc461a04c8bf
Gal <gal.portnoy11@gmail.com>
2025-05-14 22:42:09 +0300

    added some comments

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/todolistDB').then(() => {
 const app = express();
 app.use(express.json()); 
 
+// route to category and bookmark
 app.use("/category", categoryRoutes);
 app.use("/bookmark", bookmarkRoutes);
 
