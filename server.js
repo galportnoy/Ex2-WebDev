c75613f124016c61a0ded40b26260c88b2d79a41
Gal <gal.portnoy11@gmail.com>
2025-05-14 21:17:54 +0300

    fixing mongoose connect in server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
 const categoryRoutes = require('./routes/categoryRoutes');
 const bookmarkRoutes = require('./routes/bookmarkRoutes');
+
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB').then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+});
 
 const app = express();
 app.use(express.json()); 
